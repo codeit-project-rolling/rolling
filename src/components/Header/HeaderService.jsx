4bb3a9cefbd2006069fde791b6041146b71d27ea
heemoon2 <gun7741@naver.com>
2024-02-27 23:44:11 +0900

    feat: 이모지 modal

--- src/components/Header/HeaderService.jsx
+++ src/components/Header/HeaderService.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
+import EmojiModal from './EmojiModal';
 import HeaderServiceStyles from './HeaderService.module.scss';
 import addEmojiIcon from '../../assets/images/addEmoji.png';
 import arrowDownIcon from '../../assets/images/arrow_down.png';
 import shareIcon from '../../assets/images/share.png';
 
 function HeaderService() {
+  const [emojiModal, setEmojiModal] = useState(false);
+
+  const handleEmojiModalClick = () => {
+    setEmojiModal(true);
+  };
   return (
     <div className={HeaderServiceStyles.headerServiceContainer}>
       <p className={HeaderServiceStyles.toNickname}>To. Ashely Kim</p>
@@ -15,10 +23,19 @@ function HeaderService() {
         </div>
         <div className={HeaderServiceStyles.selectionBar} />
         <div className={HeaderServiceStyles.headerEmoji}>
-          <button type="button">버튼</button>
-          <button type="button">버튼</button>
-          <button type="button">버튼</button>
-          <img src={arrowDownIcon} alt="arrowDownIcon" />
+          <button type="button" className={HeaderServiceStyles.emojiBtn}>
+            버튼
+          </button>
+          <button type="button" className={HeaderServiceStyles.emojiBtn}>
+            버튼
+          </button>
+          <button type="button" className={HeaderServiceStyles.emojiBtn}>
+            버튼
+          </button>
+          <button onClick={handleEmojiModalClick} type="button" className={HeaderServiceStyles.modalIcon}>
+            <img src={arrowDownIcon} alt="arrowDownIcon" />
+          </button>
+          {emojiModal && <EmojiModal onClose={() => setEmojiModal(false)} />}
         </div>
         <div>
           <button type="button" className={HeaderServiceStyles.addEmojiBtn}>
