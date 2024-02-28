513a9a9297b354dfbe7e2a83d7f4431b14c4e1cd
gun7741 <gun7741@naver.com>
2024-02-28 14:53:50 +0900

    mock데이터 추가 및 적용

--- src/components/Header/HeaderService.jsx
+++ src/components/Header/HeaderService.jsx
@@ -7,6 +7,7 @@ import shareIcon from 'assets/images/share.png';
 import EmojiModal from 'components/Header/EmojiModal';
 import HeaderServiceStyles from 'components/Header/HeaderService.module.scss';
 import ShareModal from 'components/Header/ShareModal';
+import userData from 'components/Header/mock.json';
 
 function HeaderService() {
   const [emojiModal, setEmojiModal] = useState(false);
@@ -19,24 +20,35 @@ function HeaderService() {
   };
   return (
     <div className={HeaderServiceStyles.headerServiceContainer}>
-      <p className={HeaderServiceStyles.toNickname}>To. Ashely Kim</p>
+      <p className={HeaderServiceStyles.toNickname}>To. {userData.name}</p>
       <div className={HeaderServiceStyles.headerRight}>
         <div className={HeaderServiceStyles.howManyPerson}>
+          <div className={HeaderServiceStyles.senderProfile}>
+            {userData.recentMessages.map((profileImg) => (
+              <img
+                src={profileImg.profileImageURL}
+                alt="senderProfileImg"
+                className={HeaderServiceStyles.senderProfileImg}
+              />
+            ))}
+            <div className={HeaderServiceStyles.senderCount}>
+              +{userData.messageCount - userData.recentMessages.length}
+            </div>
+          </div>
           <p>
-            <span>N</span>명이 작성했어요!
+            <span>{userData.messageCount}</span>명이 작성했어요!
           </p>
         </div>
         <div className={HeaderServiceStyles.selectionBar} />
         <div className={HeaderServiceStyles.headerEmoji}>
-          <button type="button" className={HeaderServiceStyles.emojiBtn}>
-            버튼
-          </button>
-          <button type="button" className={HeaderServiceStyles.emojiBtn}>
-            버튼
-          </button>
-          <button type="button" className={HeaderServiceStyles.emojiBtn}>
-            버튼
-          </button>
+          {userData.topReactions.map((reaction) => (
+            <button key={reaction.id} type="button" className={HeaderServiceStyles.emojiBtn}>
+              <p>
+                {reaction.emoji}
+                {reaction.count}
+              </p>
+            </button>
+          ))}
           <button onClick={handleEmojiModalClick} type="button" className={HeaderServiceStyles.modalIcon}>
             <img src={arrowDownIcon} alt="arrowDownIcon" />
           </button>
