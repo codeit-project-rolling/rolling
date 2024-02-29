96b17a7d04f74edbf2f887d15b44cb838859120f
jinhok96 <jinhok96a@gmail.com>
2024-02-29 21:02:15 +0900

    chore: api에 GET, POST, DELETE 요청하는 커스텀 훅 추가 	new file:   src/hooks/useDeleteData.js 	new file:   src/hooks/useGetData.js 	new file:   src/hooks/usePostData.js

---
+++ src/hooks/usePostData.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const API_URL = 'https://rolling-api.vercel.app/0-3/';
+const SUCCESS = 201;
+
+function usePostData(apiEndpoint, postData) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const url = new URL(apiEndpoint, API_URL);
+        const response = await axios.post(url, postData);
+
+        if (response.status !== SUCCESS) {
+          throw new Error(`Response error with status code: ${response.status}`);
+        }
+      } catch (errorData) {
+        const errorMessage = errorData.response ? errorData.response.data : errorData.toString();
+        setError(errorMessage);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, [apiEndpoint]);
+
+  return { loading, error };
+}
+
+export default usePostData;
