dc4658badd0bf20bebbf7e7a295cbaccf7a293a4
minbo <smb5174@naver.com>
2024-02-29 18:34:00 +0900

    refactor: children을 나누지 않고 하나로 출력

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { ReactComponent as Icon } from 'assets/images/smile.svg';
+
+import Button from 'components/Button/Button';
 import './App.scss';
 
 function App() {
-  return <div className="App"></div>;
+  const handleClick = () => {
+    console.log(1);
+  };
+  return (
+    <div className="App">
+      <Button buttonType="outlined40" onClick={handleClick}>
+        <p>111</p>
+        <Icon fill="black" />
+      </Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,46 +1,70 @@
 import classNames from 'classnames';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
 
 import styles from 'components/Button/Button.module.scss';
 
-function Practice({ buttonType, children, disabled, onClick }) {
+function Button({ buttonType, children, disabled, onClick }) {
   const buttonClassName = classNames({
     [styles.primaryButton56]: buttonType === 'primary56',
     [styles.primaryButton40]: buttonType === 'primary40',
     [styles.secondaryButton40]: buttonType === 'secondary40',
     [styles.outlinedButton56]: buttonType === 'outlined56',
-    [styles.outlinedButton40]: buttonType === 'outlined40' && children.length !== 2,
-    [styles.outlinedSmileButton40]: buttonType === 'outlined40' && children.length === 2,
-    [styles.outlinedButton36]: buttonType === 'outlined36' && typeof children[0] !== 'string' && children.length !== 2,
-    [styles.outlinedSmileButton36]: buttonType === 'outlined36' && children.length === 2,
-    [styles.outlinedButton28]: buttonType === 'outlined28' && typeof children[0] !== 'string' && children.length !== 2,
-    [styles.outlinedSmileButton28]: buttonType === 'outlined28' && children.length === 2,
+    [styles.outlinedButton40]: buttonType === 'outlined40' && children.length === 'undefined',
+    [styles.outlinedSmileButton40]: buttonType === 'outlined40' && children.length !== 'undefined',
+    [styles.outlinedButton36]:
+      buttonType === 'outlined36' && typeof children[0] !== 'string' && children.length === 'undefined',
+    [styles.outlinedSmileButton36]: buttonType === 'outlined36' && children.length !== 'undefined',
+    [styles.outlinedButton28]:
+      buttonType === 'outlined28' && typeof children[0] !== 'string' && children.length === 'undefined',
+    [styles.outlinedSmileButton28]: buttonType === 'outlined28' && children.length !== 'undefined',
     [styles.outlinedSingleButton36]:
-      buttonType === 'outlined36' && typeof children[0] !== 'string' && children.length !== 2,
-    [styles.outlinedSingleButton28]: buttonType === 'outlined28' && children[0] !== string && children.length !== 2,
+      buttonType === 'outlined36' && typeof children !== 'string' && children.length === 'undefined',
+    [styles.outlinedSingleButton28]:
+      buttonType === 'outlined28' && typeof children !== 'string' && children.length === 'undefined',
     [styles.disabled]: disabled,
   });
 
-  const handleClick = () => onClick();
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      if (buttonRef.current) {
+        buttonRef.current.blur();
+      }
+    };
+
+    document.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, []);
+
+  const handleClick = () => {
+    if (!disabled) {
+      onClick();
+    }
+  };
 
   return (
-    <button type="button" className={buttonClassName} onClick={handleClick}>
-      <div>{children}</div>
+    <button type="button" className={buttonClassName} onClick={handleClick} ref={buttonRef}>
+      <div className={styles.basicSet}>{children}</div>
     </button>
   );
 }
 
-Practice.propTypes = {
+Button.propTypes = {
   children: PropTypes.string,
   buttonType: PropTypes.string,
   disabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
 };
 
-Practice.defaultProps = {
+Button.defaultProps = {
   children: '',
   buttonType: '',
   disabled: false,
 };
 
-export default Practice;
+export default Button;
