6e3537e02d2a603971e68d42805f3b997dd5b00d
gun7741 <gun7741@naver.com>
2024-02-29 22:37:13 +0900

    chore: BackgroundOption 컴포넌트 생성

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,5 +1,25 @@
+import Header from 'components/Header/HeaderComponents/Header';
+import ToggleButton from 'components/ToggleButton/ToggleButton';
+
+import styles from 'pages/PostPage/PostPage.module.scss';
+
 function PostPage() {
-  return <div />;
+  return (
+    <div className={styles.postPageContainer}>
+      <div className={styles.headerContainer}>
+        <Header />;
+      </div>
+      <div className={styles.mainContainer}>
+        <div className={styles.recipientContainer}>
+          <p className={styles.toText}>To.</p>
+          <input className={styles.recipientNickname} placeholder="받는 사람 이름을 입력해주세요. " />
+          <p className={styles.chooseText}>배경화면을 선택해 주세요.</p>
+          <p className={styles.whichChooseText}>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
+          <ToggleButton />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PostPage;
