75dd5d7c5e850338d6ca76690c8970b5d72896ca
jinhok96 <jinhok96a@gmail.com>
2024-03-01 05:21:48 +0900

    chore: HomePage 초기 상태로 복구 	modified:   src/pages/HomePage/HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,4 @@
-import { useEffect } from 'react';
-
-import useGetRecipientList from 'hooks/useGetRecipientList';
-
 function HomePage() {
-  // return <div />;
-  const { data, loading } = useGetRecipientList();
-
-  useEffect(() => {
-    if (!loading) {
-      console.log(data);
-    }
-  }, [loading]);
-
   return <div />;
 }
 
