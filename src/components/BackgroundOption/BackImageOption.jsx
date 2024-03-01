e5d24744b2b12a429d9c5234eb7d93a09e7625f9
gun7741 <gun7741@naver.com>
2024-03-01 20:56:03 +0900

    feat: BackImageOption 컴포넌트 수정중

--- src/components/BackgroundOption/BackImageOption.jsx
+++ src/components/BackgroundOption/BackImageOption.jsx
@@ -5,26 +5,30 @@ import styles from 'components/BackgroundOption/BackImageOption.module.scss';
 import CheckButton from 'components/CheckButton/CheckButton';
 
 function BackImageOption({ onSelect }) {
-  const [selectedColor, setSelectedColor] = useState('beige');
+  const [selectedImg, setSelectedImg] = useState('beige');
 
   const handleColorSelect = (color) => {
-    setSelectedColor(color);
+    setSelectedImg(color);
     onSelect(color);
   };
 
   return (
     <div className={styles.backgroundOption}>
-      <button className={`${styles.option} ${styles.beige}`} onClick={() => handleColorSelect('beige')} type="button">
-        {selectedColor === 'beige' && <CheckButton />}
+      <button className={styles.option} onClick={() => handleColorSelect('beige')} type="button">
+        <img src="https://picsum.photos/id/683/3840/2160" alt="Beige" className={styles.image} />
+        {selectedImg === 'beige' && <CheckButton />}
       </button>
       <button className={`${styles.option} ${styles.purple}`} onClick={() => handleColorSelect('purple')} type="button">
-        {selectedColor === 'purple' && <CheckButton />}
+        {selectedImg === 'purple' && <CheckButton />}
+        <img src="https://picsum.photos/id/683/3840/2160" alt="Purple" className={styles.image} />
       </button>
       <button className={`${styles.option} ${styles.blue}`} onClick={() => handleColorSelect('blue')} type="button">
-        {selectedColor === 'blue' && <CheckButton />}
+        {selectedImg === 'blue' && <CheckButton />}
+        <img src="https://picsum.photos/id/683/3840/2160" alt="Blue" className={styles.image} />
       </button>
-      <button className={`${styles.option}  ${styles.green}`} onClick={() => handleColorSelect('green')} type="button">
-        {selectedColor === 'green' && <CheckButton />}
+      <button className={`${styles.option} ${styles.green}`} onClick={() => handleColorSelect('green')} type="button">
+        {selectedImg === 'green' && <CheckButton />}
+        <img src="https://picsum.photos/id/683/3840/2160" alt="Green" className={styles.image} />
       </button>
     </div>
   );
