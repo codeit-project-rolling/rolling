82cf82ed8d5c2ca5172fed88300410084bd73ae0
minbo <smb5174@naver.com>
2024-03-01 17:48:58 +0900

    fix: Hr이 안보이는 문제 해결

--- src/components/Header/HeaderComponents/Header.jsx
+++ src/components/Header/HeaderComponents/Header.jsx
@@ -1,25 +1,24 @@
 import LogoImg from 'assets/images/Logo.png';
 
-// import Button from 'components/Button/Button';
+import Button from 'components/Button/Button';
 import HeaderStyles from 'components/Header/HeaderComponents/Header.module.scss';
 
 function Header() {
   return (
-    <div className={HeaderStyles.headerContainer}>
-      <div className={HeaderStyles.headerTop}>
-        <div className={HeaderStyles.headerLogo}>
-          <img src={LogoImg} alt="LogoImg" />
-          <p className={HeaderStyles.headerTitle}>Rolling</p>
+    <>
+      <div className={HeaderStyles.headerContainer}>
+        <div className={HeaderStyles.headerTop}>
+          <div className={HeaderStyles.headerLogo}>
+            <img src={LogoImg} alt="LogoImg" />
+            <p className={HeaderStyles.headerTitle}>Rolling</p>
+          </div>
+          <Button buttonType="outlined40">
+            <p>롤링 페이퍼 만들기</p>
+          </Button>
         </div>
-        <button type="button" className={HeaderStyles.addRollingBtn}>
-          롤링 페이퍼 만들기
-        </button>
-        {/* <Button buttonType="outlined40">
-          <p>롤링 페이퍼 만들기</p>
-        </Button> */}
       </div>
       <hr className={HeaderStyles.line} />
-    </div>
+    </>
   );
 }
 export default Header;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,15 @@
 import HeaderLayout from 'components/Header/HeaderLayout';
+import Card from 'components/card/card';
 
 function HomePage() {
-  return <HeaderLayout />;
+  return (
+    <>
+      <HeaderLayout />
+      <div>
+        <Card />
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
