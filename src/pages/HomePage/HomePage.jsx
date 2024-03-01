31a7607f57cb53ae07bd3871fa5876f2e05d4885
minbo <smb5174@naver.com>
2024-03-01 23:33:55 +0900

    feat: 카드 목록 영역의 너비는 1200px로 고정하고 좌우 여백만 변하다가, 화면의 너비가 1248px보다 줄어들 때 좌우 여백은 24px로 고정하고 카드들의 너비가 줄어들게 구현

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,44 @@
-import useGetRecipientList from 'hooks/useGetRecipientList';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import useGetMessageList from 'hooks/useGetMessageList';
+// import useGetRecipient from 'hooks/useGetRecipient';
+// import useGetRecipientList from 'hooks/useGetRecipientList';
 
 import HeaderLayout from 'components/Header/HeaderLayout';
+import PlusButton from 'components/PlusButton/PlusButton';
 import Card from 'components/card/card';
 
 import styles from 'pages/HomePage/HomePage.module.scss';
 
 function HomePage() {
-  const { data } = useGetRecipientList();
-  console.log(data);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [exportData, setExportData] = useState([]);
+
+  const { data } = useGetMessageList({ id: 3058 });
+  const handleClick = () => {
+    navigate('/post/3058/message');
+  };
+
+  useEffect(() => {
+    if (data) {
+      setExportData(data.results);
+      setLoading(false);
+    }
+  }, [data]);
+
   return (
     <>
       <HeaderLayout />
       <div className={styles.heightCover} />
       <div className={styles.cardListContainer}>
-        <Card data={data} />
+        <div className={styles.cardList}>
+          <div className={styles.basicButton}>
+            <PlusButton onClick={handleClick} />
+          </div>
+          {loading ? <div>Loading...</div> : exportData?.map((item) => <Card key={item.id} data={item} />)}
+        </div>
       </div>
     </>
   );
