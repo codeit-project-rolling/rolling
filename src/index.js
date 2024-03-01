b9b7fb039cbb9b3b7815e4e858cd928a1712d7b4
jinhok96 <jinhok96a@gmail.com>
2024-03-02 03:49:16 +0900

    chore: 모달이 추가될 위치 설정 	modified:   public/index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import './index.scss';
+import ReactModal from 'react-modal';
 
 import App from './App';
 
+import './index.scss';
+
+// 모달이 렌더링되면 #modal-root를 제외한 다른 곳에 상호작용 금지
+ReactModal.setAppElement('#modal-root');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
