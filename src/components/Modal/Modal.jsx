d1ba4254609858f647917b75c81b1013dad29207
jinhok96 <jinhok96a@gmail.com>
2024-03-02 05:52:16 +0900

    chore: onSubmit prop 제거 	modified:   src/components/Modal/Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,17 +11,8 @@ import formatDate from 'utils/formatDate';
 // message = {id, recipientId, sender, profileImageURL, relationship, content, font, createdAt}
 function Modal({ message, onClose }) {
   const { sender, profileImageURL, relationship, content, createdAt } = message;
-
   const createDate = formatDate(createdAt);
 
-  // ReactModal 속성 설명
-  // _1. isOpen: 모달이 열려있는지 여부를 결정하는 불리언 값입니다. 필수 prop입니다.
-  // _2. className: 모달 콘텐츠에 적용할 CSS 클래스입니다.
-  // _3. overlayClassName: 모달 오버레이에 적용할 CSS 클래스입니다.
-  // _4. onRequestClose: 모달을 닫으려는 요청이 있을 때 실행될 콜백 함수입니다. 예를 들어, 오버레이 클릭이나 키보드 ESC 키 입력 등이 있습니다.
-  // _5. shouldCloseOnOverlayClick: 오버레이를 클릭했을 때 모달을 닫을지 여부를 결정합니다.
-  // _6. shouldCloseOnEsc: ESC 키를 눌렀을 때 모달을 닫을지 여부를 결정합니다.
-  // _7. parentSelector: 모달을 렌더링할 부모 요소를 선택하는 함수입니다.
   return (
     <ReactModal
       isOpen
