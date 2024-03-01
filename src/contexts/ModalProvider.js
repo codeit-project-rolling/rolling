c74df376c1bdf822ce6cb54e5dbb21ec189cf8bb
jinhok96 <jinhok96a@gmail.com>
2024-03-02 04:20:53 +0900

    chore: ModalProvider 추가 	new file:   src/contexts/ModalProvider.js

---
+++ src/contexts/ModalProvider.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import { useState, useMemo } from 'react';
+
+import ModalComponent from './ModalComponent';
+import { ModalStateContext } from './ModalContext';
+
+// 현재 열려있는 모달 상태 관리
+// children에 ModalProvider에서 Provider로 전달한 값 공유
+export default function ModalProvider({ children }) {
+  // 열려있는 모달 저장
+  const [openedModal, setOpenedModal] = useState();
+
+  // 모달 열기
+  // Modal: 열고싶은 모달 컴포넌트
+  // propList: 모달 컴포넌트로 넘겨주는 propList
+  const open = (Modal, propList) => {
+    if (!Modal) return null;
+    if (openedModal) return null;
+
+    const newModal = { Modal, propList };
+
+    return setOpenedModal(newModal);
+  };
+
+  // 모달 닫기
+  const close = () => {
+    setOpenedModal(null);
+  };
+
+  // useMemo: {open, close} 객체 저장
+  // 저장된 객체는 첫 렌더링될 때 생성, 이후 의존성 배열의 값이 변경될 때 리렌더링 후 재생성
+  // 의존성 배열이 []으로 비어있으므로 첫 렌더링 이후 재생성되지 않음
+  const dispatch = useMemo(() => ({ open, close }), []);
+
+  return (
+    <ModalStateContext.Provider value={openedModal}>
+      <ModalProvider.Provider value={dispatch}>
+        {children}
+        {openedModal && <ModalComponent />}
+      </ModalProvider.Provider>
+    </ModalStateContext.Provider>
+  );
+}
+
+ModalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
