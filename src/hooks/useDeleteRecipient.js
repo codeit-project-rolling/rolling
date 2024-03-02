1a8d11d7345f3aef31f1ef1d9d9f1268fc33a10b
jinhok96 <jinhok96a@gmail.com>
2024-03-02 16:48:56 +0900

    refactor: useDeleteRecipient 개선 	modified:   src/hooks/useDeleteRecipient.js

--- src/hooks/useDeleteRecipient.js
+++ src/hooks/useDeleteRecipient.js
@@ -1,4 +1,6 @@
-import apiDelete from 'apis/apiDelete';
+import { useEffect, useState } from 'react';
+
+import createApiRequest from 'apis/createApiRequest';
 
 // Github Wiki: API 명세 1-6) 롤링 페이퍼 대상 삭제
 // id: integer required
@@ -25,10 +27,21 @@ function useDeleteRecipient({ id } = {}) {
     return { loading: false, error: errorMessage };
   }
 
-  // apiDelete
+  // api 요청
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const apiEndpoint = `${TEAM}/recipients/${id}/`;
 
-  const { loading, error } = apiDelete(apiEndpoint);
+  useEffect(async () => {
+    try {
+      await createApiRequest().delete(apiEndpoint);
+    } catch (errorData) {
+      setError(errorData);
+    } finally {
+      setLoading(false);
+    }
+  }, [apiEndpoint]);
 
   return { loading, error };
 }
