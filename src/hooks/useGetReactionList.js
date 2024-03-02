4c6c9ecaa497f9dd206fcb5f9894279c01036697
jinhok96 <jinhok96a@gmail.com>
2024-03-02 16:41:16 +0900

    refactor: useGetReactionList 개선 	modified:   src/hooks/useGetReactionList.js

--- src/hooks/useGetReactionList.js
+++ src/hooks/useGetReactionList.js
@@ -1,4 +1,6 @@
-import apiGet from 'apis/apiGet';
+import { useEffect, useState } from 'react';
+
+import createApiRequest from 'apis/createApiRequest';
 
 // Github Wiki: API 명세 3-4) 대상에게 단 리액션 조회
 // id: integer required
@@ -29,7 +31,11 @@ function useGetReactionList({ id, limit, offset }) {
     return { data: null, loading: false, error: errorMessage };
   }
 
-  // apiGet
+  // api 요청
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   // URLSearchParams 사용
   const queryParams = new URLSearchParams();
 
@@ -39,7 +45,16 @@ function useGetReactionList({ id, limit, offset }) {
   const queryString = queryParams.toString();
   const apiEndpoint = `${TEAM}/recipients/${id}/reactions/${queryString ? `?${queryString}` : ''}`;
 
-  const { data, loading, error } = apiGet(apiEndpoint);
+  useEffect(async () => {
+    try {
+      const response = await createApiRequest().get(apiEndpoint);
+      setData(response?.data);
+    } catch (errorData) {
+      setError(errorData);
+    } finally {
+      setLoading(false);
+    }
+  }, [apiEndpoint]);
 
   return { data, loading, error };
 }
