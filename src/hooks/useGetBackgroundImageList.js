9b96ffb3940fa6168ab623873f95f4adf76fe173
jinhok96 <jinhok96a@gmail.com>
2024-03-02 14:55:30 +0900

    chore: useGetBackgroundImageList 추가 	new file:   src/hooks/useGetBackgroundImageList.js

---
+++ src/hooks/useGetBackgroundImageList.js
@@ -0,0 +1,14 @@
+import apiGet from 'apis/apiGet';
+
+// background-images 배열 받아오는 함수
+
+function useGetBackgroundImageList() {
+  // apiGet
+  const apiEndpoint = `profile-images/`;
+
+  const { data, loading, error } = apiGet(apiEndpoint);
+
+  return { data, loading, error };
+}
+
+export default useGetBackgroundImageList;
