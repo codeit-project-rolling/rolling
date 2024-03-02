feb913b2ab8c1018d36e51a60a5d23c2f966b0fb
gun7741 <gun7741@naver.com>
2024-03-02 14:54:33 +0900

    feat: Header link,navigate 적용

--- src/components/Header/HeaderComponents/Header.jsx
+++ src/components/Header/HeaderComponents/Header.jsx
@@ -1,17 +1,26 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 import LogoImg from 'assets/images/Logo.png';
 
 import Button from 'components/Button/Button';
 import HeaderStyles from 'components/Header/HeaderComponents/Header.module.scss';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleRollingButtonClick = () => {
+    navigate('/post');
+  };
   return (
     <div className={HeaderStyles.headerContainer}>
       <div className={HeaderStyles.headerTop}>
-        <div className={HeaderStyles.headerLogo}>
-          <img src={LogoImg} alt="LogoImg" />
-          <p className={HeaderStyles.headerTitle}>Rolling</p>
-        </div>
-        <Button buttonType="outlined40">
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <div className={HeaderStyles.headerLogo}>
+            <img src={LogoImg} alt="LogoImg" />
+            <p className={HeaderStyles.headerTitle}>Rolling</p>
+          </div>
+        </Link>
+        <Button buttonType="outlined40" onClick={handleRollingButtonClick}>
           <p>롤링 페이퍼 만들기</p>
         </Button>
       </div>
