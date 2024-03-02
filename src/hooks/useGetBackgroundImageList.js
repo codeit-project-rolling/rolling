d28fc82b8cb00b29ad04d8091e6a0b3fe0ea392b
jinhok96 <jinhok96a@gmail.com>
2024-03-02 16:42:51 +0900

    refactor: useGetBackgroundImageList 개선 	modified:   src/hooks/useGetBackgroundImageList.js

--- src/hooks/useGetBackgroundImageList.js
+++ src/hooks/useGetBackgroundImageList.js
@@ -1,12 +1,27 @@
-import apiGet from 'apis/apiGet';
+import { useEffect, useState } from 'react';
+
+import createApiRequest from 'apis/createApiRequest';
 
 // background-images 배열 받아오는 함수
 
 function useGetBackgroundImageList() {
-  // apiGet
+  // api 요청
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const apiEndpoint = `background-images/`;
 
-  const { data, loading, error } = apiGet(apiEndpoint);
+  useEffect(async () => {
+    try {
+      const response = await createApiRequest().get(apiEndpoint);
+      setData(response?.data);
+    } catch (errorData) {
+      setError(errorData);
+    } finally {
+      setLoading(false);
+    }
+  }, [apiEndpoint]);
 
   return { data, loading, error };
 }
