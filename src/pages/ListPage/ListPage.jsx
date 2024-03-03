4302864ec3026ad9651d82d2a6de53ed9e6d334c
janyong <jyr5023@gmail.com>
2024-03-03 18:04:59 +0900

    Feat: ListPage 1차 완성

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,5 +1,53 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import { useNavigate } from 'react-router-dom';
+
+import useGetRecipientList from 'hooks/useGetRecipientList';
+
+import Button from 'components/Button/Button';
+import Header from 'components/Header/HeaderComponents/Header';
+import CardSlider from 'components/Slider/CardSlider';
+import CardList from 'components/card/Cardlist';
+
+import ListCardStyle from './ListPage.module.scss';
+
 function ListPage() {
-  return <div />;
+  const { data: likeList } = useGetRecipientList({
+    sortByLike: true,
+  });
+  const { data: recentList } = useGetRecipientList();
+  const navigate = useNavigate();
+  const handleMoveLink = () => {
+    navigate('/post');
+  };
+
+  return (
+    <div className={ListCardStyle.container}>
+      <Header />
+      <div className={ListCardStyle.cardSection}>
+        <div className={ListCardStyle.title}>인기 롤링 페이퍼 🔥</div>
+        <CardSlider itemsPerPage={4}>
+          {likeList?.results?.map((v) => (
+            <CardList data={v} />
+          ))}
+        </CardSlider>
+      </div>
+      <div className={ListCardStyle.cardSection}>
+        <div className={ListCardStyle.title}>최근에 만든 롤링 페이퍼 ⭐️️</div>
+        <CardSlider itemsPerPage={4}>
+          {recentList?.results?.map((v) => (
+            <CardList data={v} />
+          ))}
+        </CardSlider>
+      </div>
+      <div className={ListCardStyle.buttonBox}>
+        <Button className={ListCardStyle.button} buttonType="primary56" onClick={handleMoveLink}>
+          <p>나도 만들어보기</p>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ListPage;
