d400b842e68d4ae5c3b577ac7575fbca7a31322e
gun7741 <gun7741@naver.com>
2024-03-03 15:51:21 +0900

    feat: HeaderService mock데이터 -> get data

--- src/components/Header/HeaderComponents/HeaderService.jsx
+++ src/components/Header/HeaderComponents/HeaderService.jsx
@@ -1,55 +1,72 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import arrowDownIcon from 'assets/images/arrow_down.png';
 import { ReactComponent as ShareImg } from 'assets/images/share.svg';
 import { ReactComponent as SmileImg } from 'assets/images/smile.svg';
+
+import useGetRecipient from 'hooks/useGetRecipient';
 
 import BadgeEmoji from 'components/BadgeEmoji/BadgeEmoji';
 import Button from 'components/Button/Button';
 import EmojiDropdown from 'components/Header/HeaderComponents/EmojiDropdown';
 import HeaderServiceStyles from 'components/Header/HeaderComponents/HeaderService.module.scss';
 import ShareDropdown from 'components/Header/HeaderComponents/ShareDropdown';
-import userData from 'components/Header/mock.json';
+// import userData from 'components/Header/mock.json';
 
 function HeaderService() {
   const [emojiDropdown, setEmojiDropdown] = useState(false);
   const [shareDropdown, setShareDropdown] = useState(false);
-
+  const [recipientData, setRecipienData] = useState([]);
+  const { data } = useGetRecipient({ id: 3058 });
+  useEffect(() => {
+    if (data) {
+      setRecipienData(data);
+      console.log(recipientData.name);
+    }
+  }, [data]);
   return (
     <div className={HeaderServiceStyles.headerServiceContainer}>
       <div className={HeaderServiceStyles.headerContainer}>
-        <p className={HeaderServiceStyles.toNickname}>To. {userData.name}</p>
+        <p className={HeaderServiceStyles.toNickname}>To. {recipientData.name}</p>
         <div className={HeaderServiceStyles.headerRight}>
           <div className={HeaderServiceStyles.howManyPerson}>
             <div className={HeaderServiceStyles.senderProfile}>
-              {userData.recentMessages.slice(0, 3).map((message) => (
-                <img
-                  src={message.profileImageURL}
-                  alt="senderProfileImg"
-                  className={HeaderServiceStyles.senderProfileImg}
-                  key={message.id}
-                />
-              ))}
+              {recipientData &&
+                recipientData.recentMessages &&
+                recipientData.recentMessages.length > 0 &&
+                recipientData.recentMessages
+                  .slice(0, 3)
+                  .map((message) => (
+                    <img
+                      src={message.profileImageURL}
+                      alt="senderProfileImg"
+                      className={HeaderServiceStyles.senderProfileImg}
+                      key={message.id}
+                    />
+                  ))}
               <div className={HeaderServiceStyles.senderCount}>
-                +{userData.messageCount > 3 ? userData.messageCount - 3 : 0}
+                +{recipientData.messageCount > 3 ? recipientData.messageCount - 3 : 0}
               </div>
             </div>
             <p>
-              <span>{userData?.messageCount}</span>명이 작성했어요!
+              <span>{recipientData?.messageCount}</span>명이 작성했어요!
             </p>
           </div>
           <div className={HeaderServiceStyles.selectionBar} />
           <div className={HeaderServiceStyles.headerEmoji}>
-            {userData.topReactions.slice(0, 3).map((reaction) => (
-              <BadgeEmoji key={reaction.id} emoji={reaction?.emoji} count={reaction?.count} />
-            ))}
+            {recipientData &&
+              recipientData.topReactions &&
+              recipientData.topReactions.length > 0 &&
+              recipientData.topReactions
+                .slice(0, 3)
+                .map((reaction) => <BadgeEmoji key={reaction.id} emoji={reaction?.emoji} count={reaction?.count} />)}
             <button
               onClick={() => setEmojiDropdown(!emojiDropdown)}
               type="button"
               className={HeaderServiceStyles.modalIcon}
             >
               <img src={arrowDownIcon} alt="arrowDownIcon" />
-              {emojiDropdown && <EmojiDropdown emojiList={userData.topReactions} />}
+              {emojiDropdown && <EmojiDropdown emojiList={recipientData?.topReactions || []} />}
             </button>
           </div>
           <div>
