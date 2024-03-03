056374c7fe35c0a9f1c1dbf4b965e9b302b5d44a
gun7741 <gun7741@naver.com>
2024-03-03 21:34:01 +0900

    feat: Post/{id} 페이지에서 롤링 페이퍼 만들기 버튼 조건부 렌더링

--- src/components/Header/HeaderComponents/Header.jsx
+++ src/components/Header/HeaderComponents/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import LogoImg from 'assets/images/Logo.png';
 
@@ -7,9 +7,11 @@ import HeaderStyles from 'components/Header/HeaderComponents/Header.module.scss'
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
   const handleRollingButtonClick = () => {
     navigate('/post');
   };
+  const id = '3058';
   return (
     <>
       <div className={HeaderStyles.headerContainer}>
@@ -20,9 +22,11 @@ function Header() {
               <p className={HeaderStyles.headerTitle}>Rolling</p>
             </div>
           </Link>
-          <Button buttonType="outlined40" onClick={handleRollingButtonClick}>
-            <p>롤링 페이퍼 만들기</p>
-          </Button>
+          {location.pathname !== `/post/${id}` && ( // 현재 경로가 '/post/:id'가 아닐 때에만 렌더링
+            <Button buttonType="outlined40" onClick={handleRollingButtonClick}>
+              <p>롤링 페이퍼 만들기</p>
+            </Button>
+          )}
         </div>
       </div>
       <hr className={HeaderStyles.line} />
