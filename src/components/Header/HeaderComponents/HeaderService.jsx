88a8ec1984251d161ec8ddf0c6856c8e56531cac
gun7741 <gun7741@naver.com>
2024-03-03 15:05:07 +0900

    style: Header컴포넌트 수정

--- src/components/Header/HeaderComponents/HeaderService.jsx
+++ src/components/Header/HeaderComponents/HeaderService.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 
-// import addEmojiIcon from 'assets/images/addEmoji.png';
 import arrowDownIcon from 'assets/images/arrow_down.png';
 import { ReactComponent as ShareImg } from 'assets/images/share.svg';
 import { ReactComponent as SmileImg } from 'assets/images/smile.svg';
@@ -18,55 +17,58 @@ function HeaderService() {
 
   return (
     <div className={HeaderServiceStyles.headerServiceContainer}>
-      <p className={HeaderServiceStyles.toNickname}>To. {userData.name}</p>
-      <div className={HeaderServiceStyles.headerRight}>
-        <div className={HeaderServiceStyles.howManyPerson}>
-          <div className={HeaderServiceStyles.senderProfile}>
-            {userData.recentMessages.slice(0, 3).map((message) => (
-              <img
-                src={message.profileImageURL}
-                alt="senderProfileImg"
-                className={HeaderServiceStyles.senderProfileImg}
-              />
-            ))}
-            <div className={HeaderServiceStyles.senderCount}>
-              +{userData.messageCount > 3 ? userData.messageCount - 3 : 0}
+      <div className={HeaderServiceStyles.headerContainer}>
+        <p className={HeaderServiceStyles.toNickname}>To. {userData.name}</p>
+        <div className={HeaderServiceStyles.headerRight}>
+          <div className={HeaderServiceStyles.howManyPerson}>
+            <div className={HeaderServiceStyles.senderProfile}>
+              {userData.recentMessages.slice(0, 3).map((message) => (
+                <img
+                  src={message.profileImageURL}
+                  alt="senderProfileImg"
+                  className={HeaderServiceStyles.senderProfileImg}
+                  key={message.id}
+                />
+              ))}
+              <div className={HeaderServiceStyles.senderCount}>
+                +{userData.messageCount > 3 ? userData.messageCount - 3 : 0}
+              </div>
             </div>
+            <p>
+              <span>{userData?.messageCount}</span>명이 작성했어요!
+            </p>
           </div>
-          <p>
-            <span>{userData?.messageCount}</span>명이 작성했어요!
-          </p>
-        </div>
-        <div className={HeaderServiceStyles.selectionBar} />
-        <div className={HeaderServiceStyles.headerEmoji}>
-          {userData.topReactions.slice(0, 3).map((reaction) => (
-            <BadgeEmoji key={reaction.id} emoji={reaction?.emoji} count={reaction?.count} />
-          ))}
-          <button
-            onClick={() => setEmojiDropdown(!emojiDropdown)}
-            type="button"
-            className={HeaderServiceStyles.modalIcon}
+          <div className={HeaderServiceStyles.selectionBar} />
+          <div className={HeaderServiceStyles.headerEmoji}>
+            {userData.topReactions.slice(0, 3).map((reaction) => (
+              <BadgeEmoji key={reaction.id} emoji={reaction?.emoji} count={reaction?.count} />
+            ))}
+            <button
+              onClick={() => setEmojiDropdown(!emojiDropdown)}
+              type="button"
+              className={HeaderServiceStyles.modalIcon}
+            >
+              <img src={arrowDownIcon} alt="arrowDownIcon" />
+              {emojiDropdown && <EmojiDropdown emojiList={userData.topReactions} />}
+            </button>
+          </div>
+          <div>
+            <Button buttonType="outlined36">
+              <SmileImg fill="black" />
+              <p>추가</p>
+            </Button>
+          </div>
+          <div className={HeaderServiceStyles.selectionBar} />
+          <Button
+            buttonType="outlined36"
+            onClick={() => {
+              setShareDropdown(!shareDropdown);
+            }}
           >
-            <img src={arrowDownIcon} alt="arrowDownIcon" />
-            {emojiDropdown && <EmojiDropdown emojiList={userData.topReactions} />}
-          </button>
-        </div>
-        <div>
-          <Button buttonType="outlined36">
-            <SmileImg fill="black" />
-            <p>추가</p>
+            <ShareImg fill="black" />
           </Button>
+          {shareDropdown && <ShareDropdown />}
         </div>
-        <div className={HeaderServiceStyles.selectionBar} />
-        <Button
-          buttonType="outlined36"
-          onClick={() => {
-            setShareDropdown(!shareDropdown);
-          }}
-        >
-          <ShareImg fill="black" />
-        </Button>
-        {shareDropdown && <ShareDropdown />}
       </div>
     </div>
   );
