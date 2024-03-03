1f37d37591d81328df709f3861567a0b58b514d3
minbo <smb5174@naver.com>
2024-03-03 12:06:11 +0900

    fix: App.js 파일에서 edit과 message페이지 경로 수정

--- src/pages/PostPage/PostIdPage/PostIdPage.jsx
+++ src/pages/PostPage/PostIdPage/PostIdPage.jsx
@@ -1,7 +1,50 @@
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import useGetMessageList from 'hooks/useGetMessageList';
+
 import HeaderLayout from 'components/Header/HeaderLayout';
+import PlusButton from 'components/PlusButton/PlusButton';
+import Card from 'components/card/card';
+
+import styles from 'pages/PostPage/PostIdPage/PostIdPage.module.scss';
 
 function PostIdPage() {
-  return <HeaderLayout />;
+  const buttonAndCardCombinedClass = classNames(styles.basicButton, styles.card);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [exportData, setExportData] = useState([]);
+
+  const { data } = useGetMessageList({ id: 3058 });
+  const handleClick = () => {
+    navigate('/post/3058/message');
+  };
+  useEffect(() => {
+    if (data) {
+      setExportData(data.results);
+      setLoading(false);
+    }
+  }, [data]);
+
+  return (
+    <>
+      <HeaderLayout />
+      <div className={styles.heightCover} />
+      <div className={styles.cardListContainer}>
+        <div className={styles.cardList}>
+          <div className={buttonAndCardCombinedClass}>
+            <PlusButton onClick={handleClick} />
+          </div>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            exportData?.map((item) => <Card className={styles.card} key={item.id} data={item} />)
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PostIdPage;
