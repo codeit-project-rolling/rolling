f496d7c9b2348b5ae0c1fae98a49712d7cf41378
janyong <jyr5023@gmail.com>
2024-03-04 20:24:25 +0900

    feat: ListPage에 스켈레톤 기능 추가

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable import/no-extraneous-dependencies */
 
+// import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import useGetRecipientList from 'hooks/useGetRecipientList';
@@ -8,39 +9,53 @@ import useGetRecipientList from 'hooks/useGetRecipientList';
 import Button from 'components/Button/Button';
 import Header from 'components/Header/HeaderComponents/Header';
 import CardSlider from 'components/Slider/CardSlider';
+import CardSkeleton from 'components/Slider/Skeleton';
 import CardList from 'components/card/Cardlist';
 
 import ListCardStyle from './ListPage.module.scss';
 
 function ListPage() {
-  const { data: likeList } = useGetRecipientList({
+  const { data: likeList, loading } = useGetRecipientList({
     sortByLike: true,
   });
-  const { data: recentList } = useGetRecipientList();
+  const { loading: recentLoding, data: recentList } = useGetRecipientList();
   const navigate = useNavigate();
   const handleMoveLink = () => {
     navigate('/post');
   };
+  // useEffect(() => {
+  //   // 임의로 로딩 상태 표현
+  //   setTimeout(() => setLoading(false), 3000);
+  // }, []);
 
   return (
     <div className={ListCardStyle.container}>
       <Header />
       <div className={ListCardStyle.cardSection}>
         <div className={ListCardStyle.title}>인기 롤링 페이퍼 🔥</div>
-        <CardSlider itemsPerPage={4}>
-          {likeList?.results?.map((v) => (
-            <CardList data={v} />
-          ))}
-        </CardSlider>
+        {loading ? (
+          <CardSkeleton data={4} />
+        ) : (
+          <CardSlider itemsPerPage={4}>
+            {likeList?.results?.map((v) => (
+              <CardList data={v} />
+            ))}
+          </CardSlider>
+        )}
       </div>
       <div className={ListCardStyle.cardSection}>
         <div className={ListCardStyle.title}>최근에 만든 롤링 페이퍼 ⭐️️</div>
-        <CardSlider itemsPerPage={4}>
-          {recentList?.results?.map((v) => (
-            <CardList data={v} />
-          ))}
-        </CardSlider>
+        {recentLoding ? (
+          <CardSkeleton data={4} />
+        ) : (
+          <CardSlider itemsPerPage={4}>
+            {recentList?.results?.map((v) => (
+              <CardList data={v} />
+            ))}
+          </CardSlider>
+        )}
       </div>
+
       <div className={ListCardStyle.buttonBox}>
         <Button className={ListCardStyle.button} buttonType="primary56" onClick={handleMoveLink}>
           <p>나도 만들어보기</p>
