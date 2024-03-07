7723b43c6d52210b7a2418a34105d9080f6650e8
minbo <smb5174@naver.com>
2024-03-07 11:20:14 +0900

    chore: messagepage 수정

--- src/pages/PostPage/PostIdPage/MessagePage/MessagePage.jsx
+++ src/pages/PostPage/PostIdPage/MessagePage/MessagePage.jsx
@@ -1,17 +1,5 @@
-import { useState } from 'react';
-
-import TextEditor from 'components/TextEditor/TextEditor';
-
 function MessagePage() {
-  // eslint-disable-next-line no-unused-vars
-  const [editorContent, setEditorContent] = useState('');
-
-  const handleEditorChange = (content) => {
-    setEditorContent(content);
-    // console.log(content);
-  };
-  console.log(editorContent);
-  return <TextEditor onContentChange={handleEditorChange} />;
+  return <div />;
 }
 
 export default MessagePage;
