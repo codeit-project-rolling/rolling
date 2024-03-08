9eb6b4f943cbc216ba5ffe63cbbadf78efd9124e
janyong <jyr5023@gmail.com>
2024-03-08 16:37:31 +0900

    feat: 사이드 메뉴바

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable import/no-extraneous-dependencies */
 
 // import { useEffect, useState } from 'react';
+// import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import useGetRecipientList from 'hooks/useGetRecipientList';
 
 import Button from 'components/Button/Button';
 import Header from 'components/Header/HeaderComponents/Header';
+import SideBar from 'components/SideBar/SideBar';
 import CardSlider from 'components/Slider/CardSlider';
 import CardSkeleton from 'components/Slider/Skeleton';
 import CardList from 'components/card/cardlist';
@@ -15,16 +19,21 @@ import CardList from 'components/card/cardlist';
 import ListCardStyle from './ListPage.module.scss';
 
 function ListPage() {
+  // const [isOpen, setIsOpen] = useState(false);
   const { data: likeList, loading } = useGetRecipientList({
     sortByLike: true,
   });
-
   const { loading: recentLoding, data: recentList } = useGetRecipientList();
   const navigate = useNavigate();
+
   const handleMoveLink = () => {
     // e.stopPropagation();
     navigate('/post');
   };
+
+  // const handeltoggleMenu = () => {
+  //   setIsOpen(!isOpen);
+  // };
 
   return (
     <div className={ListCardStyle.container}>
@@ -59,6 +68,18 @@ function ListPage() {
           <p>나도 만들어보기</p>
         </Button>
       </div>
+      {/*
+      <div className={ListCardStyle.serchButton} onClick={() => handeltoggleMenu()}>
+        검색버튼
+      </div>
+       <div className={isOpen ? ListCardStyle.serchBoxOpen : ListCardStyle.serchBox}>
+        
+        <div className={isOpen ? ListCardStyle.contentOpen : ListCardStyle.content}>
+          <div className={ListCardStyle.serchBar}>검색창</div>
+          <div>카드정보 보이기</div>
+        </div>
+      </div> */}
+      <SideBar />
     </div>
   );
 }
